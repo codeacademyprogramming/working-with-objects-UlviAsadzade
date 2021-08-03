db0bc696e3e051d2d47a4b5a17b1dd89bcb2de32
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-08-04 00:28:38 +1300

    homework

---
+++ Homework/assets/main.js
@@ -0,0 +1,111 @@
+"use strict"
+
+// Task - 1 
+
+// let user={
+//     name: "Ulvi",
+//     surname: "Asadzade",
+// }
+
+// console.log(user.name);
+// user.name= "Zulfu";
+// console.log(user.name);
+// delete user.name;
+// console.log(user.name);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Task - 2
+
+// let user={name:"Ulvi"};
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//         return false
+//     }
+//     return true;
+// }
+
+// console.log(isEmpty(user));
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Task - 3
+
+// let salaries = {
+//     Ulvi: 100,
+//     Ismayil: 250,
+//     Ilham: 190
+// }
+
+// function sumSalaries(obj) {
+//     let sum = 0;
+//  for (let key in obj) {
+//      sum += obj[key];
+//   }
+//   return sum;
+// }
+
+// console.log(sumSalaries(salaries));;
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Task - 4
+
+// let order = {
+//     name: "Pizza",
+//     price: 8,
+//     count: 3,
+// }
+
+// function multiplyNumeric(obj){
+//     for (const key in obj) {
+//         if (typeof obj[key] == 'number') {
+//             obj[key] *= 2;
+//           }
+//     }
+//     return obj;
+// }
+
+// console.log(multiplyNumeric(order));
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+// Task in class
+
+// class CustomMath{
+//     constructor(number){
+//         this.number=number;
+//     }
+
+//     add(num){
+//        return this.number += num;
+//     }
+
+//     substract(num) {
+//         return this.number -= num;
+//     }
+
+//     divide(num) {
+//        return this.number /= num;
+//     }
+
+//     multiply(num) {
+//        return this.number *= num;
+//     }
+// }
+
+// const num1 = new CustomMath(5);
+// let x = num1.add(50);
+// console.log(x);
